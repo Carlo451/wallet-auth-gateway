97d449a7c74c23aa2954d36071cbc0f29c4f5305
camo <cmvbspam@vonbutler.com>
2026-05-07 15:49:43 +0200

    add the TImeCOnfig Bean

--- src/main/java/com/camo/auth_gateway/AuthGatewayApplication.java
+++ src/main/java/com/camo/auth_gateway/AuthGatewayApplication.java
@@ -2,8 +2,12 @@ package com.camo.auth_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class AuthGatewayApplication {
 
 	public static void main(String[] args) {
